5f92a32042a4c7205be0900b1e5c2da35a75a079
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-06-22 18:04:06 -0300

    Moving XenServer classes to dedicated files for each

--- lib/fog/xen_server/connection.rb
+++ lib/fog/xen_server/connection.rb
@@ -1,19 +1,8 @@
-require 'fog/core'
-require 'fog/xml'
+require 'xmlrpc/client'
 
 module Fog
   module XenServer
-
-    class InvalidLogin < Fog::Errors::Error; end
-    class NotFound < Fog::Errors::Error; end
-    class RequestFailed < Fog::Errors::Error; end
-
-    extend Fog::Provider
-
-    service(:compute, 'Compute')
-
     class Connection
-      require 'xmlrpc/client'
 
       def initialize(host, timeout)
         @factory = XMLRPC::Client.new(host, '/')
@@ -53,32 +42,5 @@ module Fog
         end
       end
     end
-
-    class NokogiriStreamParser < XMLRPC::XMLParser::AbstractStreamParser
-
-      def initialize
-        require 'nokogiri/xml/sax/document'
-        require 'nokogiri/xml/sax/parser'
-
-        @parser_class = Class.new(Nokogiri::XML::SAX::Document) do
-
-          include XMLRPC::XMLParser::StreamParserMixin
-
-          alias_method :start_element, :startElement
-          alias_method :end_element,   :endElement
-          alias_method :characters,    :character
-          alias_method :cdata_block,   :character
-
-          def parse(str)
-            Nokogiri::XML::SAX::Parser.new(self).parse(str)
-          end
-
-        end
-      end
-
-    end
-
   end
 end
-
-
--- lib/fog/xenserver.rb
+++ lib/fog/xenserver.rb
@@ -1,2 +1,18 @@
+require 'fog/core'
+require 'fog/xml'
 require "#{File.join(File.dirname(__FILE__), 'compute')}"
 require "#{File.join(File.dirname(__FILE__), 'parsers')}"
+
+module Fog
+  module XenServer
+    autoload :Connection, 'fog/xen_server/connection'
+    autoload :InvalidLogin, 'fog/xen_server/invalid_login'
+    autoload :NokogiriStreamParser, 'fog/xen_server/nokogiri_stream_parser'
+    autoload :NotFound, 'fog/xen_server/not_found'
+    autoload :RequestFailed, 'fog/xen_server/request_failed'
+
+    extend Fog::Provider
+
+    service(:compute, 'Compute')
+  end
+end
