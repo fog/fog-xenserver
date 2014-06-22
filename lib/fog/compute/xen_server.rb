a086c9238bbfd32ead74a1101975c31994eebe1b
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-06-22 14:58:15 -0300

    Moving XenServer Service classes to dedicated files for each

--- lib/fog/compute/xen_server.rb
+++ lib/fog/compute/xen_server.rb
@@ -3,6 +3,8 @@ require 'fog/core'
 module Fog
   module Compute
     class XenServer < Fog::Service
+      autoload :Real, 'fog/compute/xen_server/real'
+      autoload :Mock, 'fog/compute/xen_server/mock'
 
       require 'fog/compute/utilities'
       require 'fog/compute/parser'
@@ -112,63 +114,6 @@ module Fog
       request :destroy_vlan
       request :snapshot_server
       request :snapshot_revert
-
-      class Real
-
-        def initialize(options={})
-          @host        = options[:xenserver_url]
-          @username    = options[:xenserver_username]
-          @password    = options[:xenserver_password]
-          @defaults    = options[:xenserver_defaults] || {}
-          @timeout     = options[:xenserver_timeout] || 30
-          @connection  = Fog::XenServer::Connection.new(@host, @timeout)
-          @connection.authenticate(@username, @password)
-        end
-
-        def reload
-          @connection.authenticate(@username, @password)
-        end
-
-        def default_template=(name)
-          @defaults[:template] = name
-        end
-
-        def default_template
-          return nil if @defaults[:template].nil?
-          (servers.custom_templates + servers.builtin_templates).find do |s|
-            (s.name == @defaults[:template]) or (s.uuid == @defaults[:template])
-          end
-        end
-
-        def default_network
-          networks.find { |n| n.name == (@defaults[:network] || "Pool-wide network associated with eth0") }
-        end
-
-      end
-
-      class Mock
-
-        def self.data
-          @data ||= Hash.new do |hash, key|
-            hash[key] = {}
-          end
-        end
-
-        def self.reset_data(keys=data.keys)
-          for key in [*keys]
-            data.delete(key)
-          end
-        end
-
-        def initialize(options={})
-          @host        = options[:xenserver_pool_master]
-          @username    = options[:xenserver_username]
-          @password    = options[:xenserver_password]
-          @connection  = Fog::XML::Connection.new(@host)
-          @connection.authenticate(@username, @password)
-        end
-
-      end
     end
   end
 end
---
+++ lib/fog/compute/xen_server/mock.rb
@@ -0,0 +1,27 @@
+module Fog
+  module Compute
+    class XenServer
+      class Mock
+        def self.data
+          @data ||= Hash.new do |hash, key|
+            hash[key] = {}
+          end
+        end
+
+        def self.reset_data(keys=data.keys)
+          for key in [*keys]
+            data.delete(key)
+          end
+        end
+
+        def initialize(options={})
+          @host        = options[:xenserver_pool_master]
+          @username    = options[:xenserver_username]
+          @password    = options[:xenserver_password]
+          @connection  = Fog::XML::Connection.new(@host)
+          @connection.authenticate(@username, @password)
+        end
+      end
+    end
+  end
+end
