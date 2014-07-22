0352d0171dd9baffbea5a6c8db707c9049226b44
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-07-22 20:39:57 -0300

    Removing Unused Parsers

--- lib/fog/parsers/xen_server.rb
+++ lib/fog/parsers/xen_server.rb
@@ -2,14 +2,7 @@ module Fog
   module Parsers
     module XenServer
       autoload :Base, 'fog/parsers/xen_server/base'
-      autoload :GetHosts, 'fog/parsers/xen_server/get_hosts'
-      autoload :GetNetworks, 'fog/parsers/xen_server/get_networks'
-      autoload :GetPools, 'fog/parsers/xen_server/get_pools'
       autoload :GetRecords, 'fog/parsers/xen_server/get_records'
-      autoload :GetStorageRepositories, 'fog/parsers/xen_server/get_storage_repositories'
-      autoload :GetVbds, 'fog/parsers/xen_server/get_vbds'
-      autoload :GetVifs, 'fog/parsers/xen_server/get_vifs'
-      autoload :GetVms, 'fog/parsers/xen_server/get_vms'
     end
   end
 end
