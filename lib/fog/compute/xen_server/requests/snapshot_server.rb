01325dc35318052af66a181240db5e60aa03d414
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-09-01 16:45:15 -0300

    Fix `snapshot` call from VM
    Using the proxy from `method_missing` on model, the request has to be named
    with vm instead of server at the end. To mantain compatibility, just an alias
    was created.

--- lib/fog/compute/xen_server/requests/snapshot_server.rb
+++ lib/fog/compute/xen_server/requests/snapshot_server.rb
@@ -5,6 +5,8 @@ module Fog
         def snapshot_server(vm_ref , name, extra_args = {})
           @connection.request({:parser => Fog::Parsers::XenServer::Base.new, :method => 'VM.snapshot'}, vm_ref, name)
         end
+
+        alias_method :snapshot_vm, :snapshot_server
       end
     end
   end
