ef487cbdeeaec9e60f62c0487c54275d2f6f805f
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-09-04 13:17:04 -0300

    Removing Object checking from requests
    Since #all_associations_and_attributes already return the reference instead of
    objects, it is not necessary to check if the param is an object or a reference.
    There are some cases (like the ones removed here) that the mentioned method
    doesn't apply, but in this cases is more a responsability of the developer to
    check then of the lib itself.
    
    In both cases it make sense to not check it anymore and thats why this commit
    removes them.

--- lib/fog/compute/xen_server/requests/clone_server.rb
+++ lib/fog/compute/xen_server/requests/clone_server.rb
@@ -3,10 +3,6 @@ module Fog
     class XenServer
       class Real
         def clone_server(server_name, template_ref)
-          if template_ref.kind_of? Fog::Compute::XenServer::Server
-            template_ref = template_ref.reference
-          end
-
           @connection.request({ :parser => Fog::Parsers::XenServer::Base.new, :method => 'VM.clone' },
                               template_ref, server_name)
         end
