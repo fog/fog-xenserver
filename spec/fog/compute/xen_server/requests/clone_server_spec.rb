23f2d621b66d8ecc4b64500ccd52633f2f93b0a6
Paulo Henrique Lopes Ribeiro <plribeiro@zertico.com>
2014-09-17 13:03:12 -0300

    Improving #clone_server spec

--- spec/fog/compute/xen_server/requests/clone_server_spec.rb
+++ spec/fog/compute/xen_server/requests/clone_server_spec.rb
@@ -1,7 +1,6 @@
 require 'minitest_helper'
 
 describe "#clone_vm" do
-
   let(:connection) do
     VCR.use_cassette('open_connection') do
       Fog::Compute.new(:provider => 'XenServer',
@@ -19,14 +18,14 @@ describe "#clone_vm" do
   before :each do
     @template = servers.first
     @template_uuid = @template.uuid
-    VCR.use_cassette('clone_template') do
-      @template.clone('Awesome Template')
+    VCR.use_cassette('clone_server') do
+      @template.clone('Awesome Server')
     end
   end
 
   it 'should create a new template' do
     @template.uuid.wont_equal(nil)
     @template.uuid.wont_equal(@template_uuid)
-    @template.name.must_equal('Awesome Template')
+    @template.name.must_equal('Awesome Server')
   end
 end
