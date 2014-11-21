093788035655d62c3d8ecf705da046c8288b7732
Celso Fernandes <fernandes@zertico.com>
2014-11-21 20:41:35 -0200

    Add messages to VM model
    Add messages keeping most similar to XenServer api, including
    parameters names
    
    Some classes were forcing some parameters to XenServer, I just left
    them needing to be explicit passed, if we define its better assume
    some default values, no problem

--- lib/fog/compute/xen_server/requests/shutdown_server.rb
+++ lib/fog/compute/xen_server/requests/shutdown_server.rb
@@ -2,23 +2,11 @@ module Fog
   module Compute
     class XenServer
       class Real
-        def shutdown_server(ref, stype = 'clean')
-          @connection.request({:parser => Fog::Parsers::XenServer::Base.new, :method => "VM.#{stype}_shutdown"}, ref)
+        def shutdown_server(ref)
+          @connection.request({ :parser => Fog::Parsers::XenServer::Base.new, :method => 'VM.shutdown' }, ref)
         end
 
         alias_method :shutdown_vm, :shutdown_server
-
-        def hard_shutdown_server(ref)
-          shutdown_server(ref, 'hard')
-        end
-
-        alias_method :hard_shutdown_vm, :hard_shutdown_server
-
-        def clean_shutdown_server(ref)
-          shutdown_server(ref, 'clean')
-        end
-
-        alias_method :clean_shutdown_vm, :clean_shutdown_server
       end
     end
   end
