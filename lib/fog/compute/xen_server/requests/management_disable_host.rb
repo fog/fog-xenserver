cbc275c6809197e9708682b57f6ff637790ad6d1
Celso Fernandes <fernandes@zertico.com>
2014-11-21 23:15:12 -0200

    Make hound happy

--- lib/fog/compute/xen_server/requests/management_disable_host.rb
+++ lib/fog/compute/xen_server/requests/management_disable_host.rb
@@ -3,7 +3,7 @@ module Fog
     class XenServer
       class Real
         def management_disable_host
-          @connection.request({ :parser => Fog::Parsers::XenServer::Base.new, :method => 'host.management_disable' })
+          @connection.request( :parser => Fog::Parsers::XenServer::Base.new, :method => 'host.management_disable' )
         end
       end
     end
