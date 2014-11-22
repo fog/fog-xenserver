937eed5406199cbbfc97984496c3b96029b06cbf
Celso Fernandes <fernandes@zertico.com>
2014-11-22 13:00:19 -0200

    New record for create_vif

--- spec/fog/compute/xen_server/requests/create_vif_spec.rb
+++ spec/fog/compute/xen_server/requests/create_vif_spec.rb
@@ -2,7 +2,7 @@ require 'minitest_helper'
 
 describe "#create_vif" do
   let(:connection) do
-    VCR.use_cassette('open_connection') do
+    VCR.use_cassette('create_vif_open_connection') do
       Fog::Compute.new(:provider => 'XenServer',
                        :xenserver_url => '192.168.10.2',
                        :xenserver_username => 'root',
@@ -10,24 +10,32 @@ describe "#create_vif" do
     end
   end
   let(:network) do
-    VCR.use_cassette('get_network_by_name') do
+    VCR.use_cassette('create_vif_get_network_by_name') do
       connection.networks.get_by_name('Pool-wide network associated with eth0')
     end
   end
-  let(:vm) do
-    VCR.use_cassette('get_vm_by_name') do
-      connection.servers.get_by_name('vm-teste')
+  let(:host) do
+    VCR.use_cassette('create_server_get_all_hosts') do
+      connection.hosts.first
     end
+  end
+  let(:vm) do
+    @server = connection.servers.new(:name => 'CrazyName')
+    @server.affinity = host
+    VCR.use_cassette('create_server_create_vm') do
+      @server.save
+    end
+    @server
   end
 
   before :each do
     @vif = connection.vifs.new
     @vif.network = network
     @vif.vm = vm
-    VCR.use_cassette('vif_set_device_number') do
+    VCR.use_cassette('create_vif_vif_set_device_number') do
       @vif.set_device_number
     end
-    VCR.use_cassette('create_vif') do
+    VCR.use_cassette('create_vif_create_vif') do
       @vif.save
     end
   end
