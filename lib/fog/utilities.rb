fe48dbc4170ed472d959b7c8210243a730bad06c
Chris O'Sullivan <thechrisoshow@gmail.com>
2017-01-05 17:19:06 +0000

    Can check for Integer class as it exists in Ruby < 2.4

--- lib/fog/utilities.rb
+++ lib/fog/utilities.rb
@@ -1,8 +1,8 @@
 class Hash
   def symbolize_keys!
     keys.each do |key|
-      if key.respond_to?(:to_sym) && !key.is_a?(0.class)
-        self[(key.to_sym rescue key)] = delete(key)
+      if key.respond_to?(:to_sym) && !key.is_a?(Integer)
+        self[key.to_sym] = delete(key)
       end
     end
     self
