58ac795bfa5f948bdfb068a32429cd6fc3d6212d
Adam Rice <development@hashnotadam.com>
2017-02-23 14:43:15 +1100

    Remove test on Fixnum since it has been removed from Ruby 2.4

--- lib/fog/utilities.rb
+++ lib/fog/utilities.rb
@@ -1,7 +1,7 @@
 class Hash
   def symbolize_keys!
     keys.each do |key|
-      self[(key.to_sym rescue key)] = delete(key) if key.respond_to?(:to_sym) && !key.is_a?(Fixnum)
+      self[key.to_sym] = delete(key) if key.respond_to?(:to_sym)
     end
     self
   end
