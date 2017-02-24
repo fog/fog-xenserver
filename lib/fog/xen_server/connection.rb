25f1feba2172d97c143cdecf85fa06a6ecc58b66
Adam Rice <development@hashnotadam.com>
2017-02-24 16:01:16 +1100

    :cop: Reduce cyclomatic complexity

--- lib/fog/xen_server/connection.rb
+++ lib/fog/xen_server/connection.rb
@@ -19,30 +19,47 @@ module Fog
 
       def request(options, *params)
         begin
-          parser   = options.delete(:parser)
-          method   = options.delete(:method)
+          parser = options.delete(:parser)
+          method = options.delete(:method)
+          response = @factory.call(method, @credentials, *request_params(params))
 
-          if params.empty?
-            response = @factory.call(method, @credentials)
-          else
-            if params.length.eql?(1) and params.first.is_a?(Hash)
-              response = @factory.call(method, @credentials, params.first)
-            elsif params.length.eql?(2) and params.last.is_a?(Array)
-              response = @factory.call(method, @credentials, params.first, params.last)
-            elsif params.length.eql?(1) and params.last.is_a?(Array)
-              response = @factory.call(method, @credentials, params.first)
-            else
-              response = eval("@factory.call('#{method}', '#{@credentials}', #{params.map {|p|  p.is_a?(String) ? "'#{p}'" : p}.join(',')})")
-            end
-          end
-          raise RequestFailed.new("#{method}: " + response["ErrorDescription"].to_s) unless response["Status"].eql? "Success"
-          if parser
-            parser.parse( response["Value"] )
-            response = parser.response
+          if !request_successful?(response)
+            raise RequestFailed.new(
+              %(#{method}: #{response["ErrorDescription"]})
+            )
           end
 
-          response
+          parsed_response(parser, response)
         end
+      end
+
+      private
+
+      def format_string_params(params)
+        params.map { |p| p.is_a?(String) ? "'#{p}'" : p }.join(",")
+      end
+
+      def parsed_response(parser, response)
+        return response unless parser
+
+        parser.parse(response["Value"])
+        parser.response
+      end
+
+      def request_params(params)
+        if params.empty?
+          []
+        elsif params.length.eql?(1) && [Hash, Array].include?(params.first.class)
+          [params.first]
+        elsif params.length.eql?(2) and params.last.is_a?(Array)
+          [params.first, params.last]
+        else
+          format_string_params(params)
+        end
+      end
+
+      def request_successful?(response)
+        response["Status"] == "Success"
       end
     end
   end
