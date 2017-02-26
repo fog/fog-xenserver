73c29114fb8b565c9ec209adf5ef9b77a911813e
Adam Rice <development@hashnotadam.com>
2017-02-26 11:20:00 +1100

    Integrate get_by_reference_or_name_or_uuid into method_missing

--- lib/fog/compute/xen_server/models/collection.rb
+++ lib/fog/compute/xen_server/models/collection.rb
@@ -16,21 +16,21 @@ module Fog
           end
 
           def method_missing(method, *args)
-            if method =~ /^(find|get)_by_(name|uuid)$/
-              return get_by(method, args[0])
+            case method
+            when :get_by_reference_or_name_or_uuid
+              query = args[0]
+              get(query) || get_by("name", query) || get_by("uuid", query)
+            when /^(find|get)_by_(name|uuid)$/
+              get_by(method, args[0])
+            else
+              super
             end
-
-            super
-          end
-
-          def get_by_reference_or_name_or_uuid(query)
-            get(query) || get_by_name(query) || get_by_uuid(query)
           end
 
           private
 
           def get_by(method, value)
-            method = method.to_s.sub('find_by_', 'get_by_')
+            method = method.to_s.sub("find_by_", "get_by_")
             ref = service.send(method, value, model.provider_class)
             return nil if ref.nil?
             get(ref)
